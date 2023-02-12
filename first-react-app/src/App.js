b197636c9e9fabd049cf2d22d2202ff78cabe73b
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2023-02-12 23:01:02 +0700

    for the first class based component

---
+++ first-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the class component</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
